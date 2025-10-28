0a0380e05abe956ca357cd48f11c0553eb6fec79
Natalia <natalia.makarchuk@sombrainc.com>
2025-10-28 13:02:55 +0200

    Update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, within } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("My dream team landing", () => {
+  test("renders header title", () => {
+    render(<App />);
+    expect(
+      screen.getByRole("heading", { level: 1, name: /my best team/i })
+    ).toBeInTheDocument();
+  });
+
+  test("renders at least one team card with name and position", () => {
+    render(<App />);
+    const card = screen.getByRole("img", { name: /john doe/i }).closest("div");
+    expect(card).toBeInTheDocument();
+
+    const utils = within(card);
+    expect(utils.getByText(/john doe/i)).toBeInTheDocument();
+    expect(utils.getByText(/software engineer/i)).toBeInTheDocument();
+  });
 });
